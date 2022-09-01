e542d6ca81bd851ffc4ae6cf9d783831063d4141
jamin12 <rudals9901@naver.com>
2022-09-01 16:55:29 +0900

    1st 수업텍스트 에디터 만들기

---
+++ 1st/sln_01/TextView/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace TextView
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다. 
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.textEdit = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.buttonEdit.Font = new System.Drawing.Font("바탕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEdit.Location = new System.Drawing.Point(292, 47);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(75, 23);
+            this.buttonEdit.TabIndex = 0;
+            this.buttonEdit.Text = "입력";
+            this.buttonEdit.UseVisualStyleBackColor = false;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // textEdit
+            // 
+            this.textEdit.AllowDrop = true;
+            this.textEdit.Location = new System.Drawing.Point(37, 47);
+            this.textEdit.Name = "textEdit";
+            this.textEdit.Size = new System.Drawing.Size(221, 21);
+            this.textEdit.TabIndex = 1;
+            this.textEdit.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textEdit_KeyPress_1);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(35, 105);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(41, 12);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "결과 : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 151);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.textEdit);
+            this.Controls.Add(this.buttonEdit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "문자 입출력";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button buttonEdit;
+		private System.Windows.Forms.TextBox textEdit;
+		private System.Windows.Forms.Label lblResult;
+	}
+}
+
---
+++ 1st/sln_01/TextView/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextView
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		string OrgStr = "";
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			OrgStr = this.lblResult.Text;
+		}
+
+		private void buttonEdit_Click(object sender, EventArgs e)
+		{
+			if (TextCheck() == true)
+			{
+				this.lblResult.Text = OrgStr + this.textEdit.Text;
+			}
+		}
+		private void textEdit_KeyPress_1(object sender, KeyPressEventArgs e)
+		{
+			if (e.KeyChar == (char)13) //엔터키를 누를 때
+			{
+				e.Handled = true; // 소리 없앰
+				if (TextCheck() == true) { this.lblResult.Text = OrgStr + this.textEdit.Text; }
+			}
+		}
+		private bool TextCheck()
+		{
+			if (this.textEdit.Text != "") return true;
+			else
+			{ 
+				MessageBox.Show("텍스트를 입력하세요!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				this.textEdit.Focus(); 
+				return false; 
+			}
+		}
+	}
+}
